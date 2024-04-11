d6aafde898d03981283b056491027bace9095daf
blue-script <alexm92@inbox.ru>
2024-04-11 12:04:51 +0500

    L-20 fix: api-key

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1/",
   withCredentials: true,
   headers: {
-    "API-KEY": "1cdd9f77-c60e-4af5-b194-659e4ebd5d41",
+    "API-KEY": "f541e741-b190-4109-afae-de12c3df8816",
   },
 })
